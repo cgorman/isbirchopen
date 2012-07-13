c524b19dec93ed8d3c4f8d91b5b666b9cc03e0f0
cgorman <chris@cgorman.net>
2012-07-13 13:01:04 -0400

    session stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Diningstatus::Application.routes.draw do
 
 
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
 
   root :to => 'locations#index'
